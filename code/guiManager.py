b1762d093073b8eda6033e3a83e23fa9674d3415
siy415 <siy415@naver.com>
2021-05-23 23:07:21 +0900

    Folder arrange
    move UI file to gui folder

--- code/guiManager.py
+++ code/guiManager.py
@@ -32,7 +32,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.__data_params['len'] = len(value)
 
         #Load the UI Page
-        uic.loadUi('midtest.ui', self)
+        uic.loadUi('./gui/midtest.ui', self)
 
         self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
         
