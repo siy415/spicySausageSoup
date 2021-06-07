1ed4a22e43e8e4200257e75a5425605351a2dc0d
JungBoKyung <84087851+JungBoKyung@users.noreply.github.com>
2021-06-08 02:18:38 +0900

    Update dataAnalazer.py
    매수매도 추천일자 반영

--- code/dataAnalazer.py
+++ code/dataAnalazer.py
@@ -52,5 +52,79 @@ for dow in l: #상승하락동일 카운트
 
     items[dow] = {'up': up_count, 'down': down_count, 'same': same_count, 'total': total_count, 
     'upval': up_countval, 'sameval': same_countval, 'downval': down_countval} #count 값 dict 저장
+    
+#매수 매도 추천일자 출력.
+SELL = []
+STAY = []
+BUY = []
+
+MON = []
+TUE = []
+WED = []
+THU = []
+FRI = []
+
+while 1:
+    #각 요일 값 가져오기
+    MONU = items['Monday'].get('upval')
+    MONS = items['Monday'].get('sameval')
+    MOND = items['Monday'].get('downval')
+
+    TUEU = items['Tuesday'].get('upval')
+    TUES = items['Tuesday'].get('sameval')
+    TUED = items['Tuesday'].get('downval')
+
+    WEDU = items['Wednesday'].get('upval')
+    WEDS = items['Wednesday'].get('sameval')
+    WEDD = items['Wednesday'].get('downval')
+
+    THUU = items['Thursday'].get('upval')
+    THUS = items['Thursday'].get('sameval')
+    THUD = items['Thursday'].get('downval')
+
+    FRIU = items['Friday'].get('upval')
+    FRIS = items['Friday'].get('sameval')
+    FRID = items['Friday'].get('downval')
+
+#분석 시작
+    if MONU == max(MONU, MONS, MOND):
+            SELL.append('월요일')
+    if MONS == max(MONU, MONS, MOND):
+            STAY.append('월요일')
+    if MOND == max(MONU, MONS, MOND):
+            BUY.append('월요일')
+
+    if TUEU == max(TUEU, TUES, TUED):
+            SELL.append('화요일')
+    if TUES == max(TUEU, TUES, TUED):
+            STAY.append('화요일')
+    if TUED == max(TUEU, TUES, TUED):
+            BUY.append('화요일')
+
+    if WEDU == max(WEDU, WEDS, WEDD):
+            SELL.append('수요일')
+    if WEDS == max(WEDU, WEDS, WEDD):
+            STAY.append('수요일')
+    if WEDD == max(WEDU, WEDS, WEDD):
+            BUY.append('수요일')
+
+    if THUU == max(THUU, THUS, THUD):
+            SELL.append('목요일')
+    if THUS == max(THUU, THUS, THUD):
+            STAY.append('목요일')
+    if THUD == max(THUU, THUS, THUD):
+            BUY.append('목요일')
+
+    if FRIU == max(FRIU, FRIS, FRID):
+            SELL.append('금요일')
+    if FRIS == max(FRIU, FRIS, FRID):
+            STAY.append('금요일')
+    if FRID == max(FRIU, FRIS, FRID):
+            BUY.append('금요일')
+    break
+
+print('매수:', SELL)
+print('관망:', STAY)
+print('매도:', BUY)
 
 print('각 일자별 카운트: ', items)
